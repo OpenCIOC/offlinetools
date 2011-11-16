9e5c6b5b65a97ab00edac638b2e1af1d3197eb69
Chris Lambacher <chris@kateandchris.net>
2011-11-16 21:39:01 +0000

    Revert back to orm and add order by. It is faster than hand coded.

--- offlinetools/views/search.py
+++ offlinetools/views/search.py
@@ -1,10 +1,9 @@
-from itertools import groupby, chain, izip, repeat
 from pyramid.view import view_config
 
 from formencode import Schema
 from sqlalchemy import and_
 from sqlalchemy.orm import aliased
-from sqlalchemy.sql.expression import case
+from sqlalchemy.sql.expression import case, collate
 
 from offlinetools import models
 from offlinetools.views.base import ViewBase
@@ -44,41 +43,11 @@ class Search(ViewBase):
         ViewType = request.user.ViewType
         session = request.dbsession
         
-        field_names = [u'ORG_LEVEL_%d' % i for i in range(1,6)] + [u'LOCATED_IN_CM']
-        field_ids = dict(session.query(models.Field.FieldName,models.Field.FieldID).filter(models.Field.FieldName.in_(field_names)).all())
-        log.debug('field_ids: %s', field_ids)
-
-        name_tmpl = '(SELECT Value FROM Record_Data WHERE bt.NUM=NUM AND LangID=? AND FieldID=?) AS ORG_LEVEL_{0}'
-
-        sql = [('''
-                SELECT bt.NUM,\n(SELECT Value FROM  Record_Data rd WHERE bt.NUM=NUM AND FieldID=? AND 
-                        LangID=(SELECT LangID FROM Record_Data WHERE rd.NUM=NUM AND FieldID=rd.FieldID 
-               ORDER BY CASE WHEN LangID=? THEN 0 ELSE 1 END, LangID LIMIT 1)) AS LOCATED_IN_CM, \n'''
-                + ',\n'.join(name_tmpl.format(i) for i in range(1,6)) + 
-               '''
-
-               FROM Record bt
-               ''')]
-
-        args = [field_ids['LOCATED_IN_CM'], LangID]
-        args.extend(chain.from_iterable(izip(repeat(LangID, 5), (field_ids[x] for x in field_names[:5]))))
-
-#        filters = [models.Record.views.any(models.View.ViewType==ViewType), 
-#                   models.Record.LangID==LangID]
-        where = ['bt.LangID=?', 'EXISTS(SELECT 1 FROM Record_Views WHERE NUM=bt.NUM AND ViewType=?)']
-        args.extend([LangID, ViewType])
-
-        quick_list = model_state.value('QuickList')
-        if quick_list:
-#            filters.append(models.Record.publications.any(models.Publication.ListID==quick_list))
-            where.append('\nEXISTS(SELECT 1 FROM Record_Publication rp WHERE rp.NUM=bt.NUM AND ListID=?)')
-            args.append(quick_list)
-
+        filters = [models.Record.views.any(models.View.ViewType==ViewType), 
+                   models.Record.LangID==LangID]
 
         if model_state.value('Terms'):
-#            filters.append(models.Record.fields.any(models.Record_Data.Value.like('%%%s%%' % model_state.value('Terms'))))
-            where.append('EXISTS(SELECT 1 from Record_Data WHERE bt.NUM=NUM AND LangID=? AND Value LIKE ?)')
-            args.extend([LangID, '%{0}%'.format(model_state.value('Terms'))])
+            filters.append(models.Record.fields.any(models.Record_Data.Value.like('%%%s%%' % model_state.value('Terms'))))
 
         community = model_state.value('Community')
         if community:
@@ -113,66 +82,36 @@ class Search(ViewBase):
 
                 log.debug('Communities: %s', session.query(models.Community.CM_ID, models.Community.ParentCommunity, models.Community_Name.Name).join(models.Community_Name).filter(models.Community_Name.LangID==0).filter(models.Community.CM_ID.in_(CM_IDS)).all())
 
-#                filters.append(models.Record.communities.any(models.Community.CM_ID.in_(CM_IDS)))
-                where.append('EXISTS(SELECT 1 FROM Record_Community WHERE bt.NUM=NUM AND CM_ID IN ({0}))'.format(','.join('?' * len(CM_IDS))))
-                args.extend(CM_IDS)
+                filters.append(models.Record.communities.any(models.Community.CM_ID.in_(CM_IDS)))
+
+        quick_list = model_state.value('QuickList')
+        if quick_list:
+            filters.append(models.Record.publications.any(models.Publication.ListID==quick_list))
 
 
+        name_args = [aliased(models.Record_Data,session.query(models.Record_Data).
+                     join(models.Field, models.Field.FieldID==models.Record_Data.FieldID).
+                     filter(models.Record_Data.LangID==LangID).
+                     filter(models.Field.FieldName==('ORG_LEVEL_%d'% x)).
+                     subquery()) for x in range(1,6)]
 
+        located_in = (aliased(models.Record_Data,session.query(models.Record_Data).
+                     join(models.Field, models.Field.FieldID==models.Record_Data.FieldID).
+                     filter(models.Record_Data.LangID==LangID).
+                     filter(models.Field.FieldName=='LOCATED_IN_CM').
+                     subquery()))
 
-#        name_args = [aliased(models.Record_Data,session.query(models.Record_Data).
-#                     join(models.Field, models.Field.FieldID==models.Record_Data.FieldID).
-#                     filter(models.Record_Data.LangID==LangID).
-#                     filter(models.Field.FieldName==('ORG_LEVEL_%d'% x)).
-#                     subquery()) for x in range(1,6)]
-#
-#        located_in = (aliased(models.Record_Data,session.query(models.Record_Data).
-#                     join(models.Field, models.Field.FieldID==models.Record_Data.FieldID).
-#                     filter(models.Record_Data.LangID==LangID).
-#                     filter(models.Field.FieldName=='LOCATED_IN_CM').
-#                     subquery()))
-#
-#        stmt = session.query(models.Record.NUM)#, located_in.Value, *[x.Value for x in name_args])
-        #stmt = stmt.outerjoin(located_in, models.Record.NUM==located_in.NUM)
-        #for substmt in name_args:
-        #    stmt = stmt.outerjoin(substmt, models.Record.NUM==substmt.NUM)
-        
-        #results = stmt.filter(and_(*filters)).all()
-        sql = ''.join(['SELECT * FROM ('] + sql + ['\nWHERE\n\t' , '\nAND\n\t'.join(where),') AS iq ORDER BY iq.ORG_LEVEL_1 COLLATE NOCASE, iq.ORG_LEVEL_2 COLLATE NOCASE, iq.ORG_LEVEL_3 COLLATE NOCASE, iq.ORG_LEVEL_4 COLLATE NOCASE, iq.ORG_LEVEL_5 COLLATE NOCASE, NUM'])
-        connection = session.connection()
-        results = map(tuple,connection.execute(sql, *args))
+        stmt = session.query(models.Record.NUM, located_in.Value, *[x.Value for x in name_args])
+        stmt = stmt.outerjoin(located_in, models.Record.NUM==located_in.NUM)
+        for substmt in name_args:
+            stmt = stmt.outerjoin(substmt, models.Record.NUM==substmt.NUM)
 
-        
+        results = stmt.filter(and_(*filters)).order_by(*([collate(x.Value, 'NOCASE') for x in name_args] + [models.Record.NUM])).all()
 
-        #fields = ['ORG_LEVEL_%d' % x for x in range(1,6)]
-#        name_data = (session.query(models.Record_Data.NUM, models.Field.FieldName, models.Record_Data.Value).
-#            join(models.Field, models.Field.FieldID==models.Record_Data.FieldID).
-#            filter(models.Record_Data.LangID==LangID).
-#            filter(models.Record_Data.NUM.in_([x[0] for x in results])).
-#            filter(models.Field.FieldName.in_(fields)).
-#            order_by(models.Record_Data.NUM)
-#           ).all()
-#
-#        stmt = aliased(session.query(models.Record_Data).subquery())
-#        located_in_data = (session.query(models.Record_Data.NUM, models.Field.FieldName, models.Record_Data.Value).
-#            join(models.Field, and_(models.Field.FieldID==models.Record_Data.FieldID,
-#            models.Record_Data.LangID==stmt.select(and_(stmt.c.NUM==models.Record_Data.NUM, stmt.c.FieldID==models.Record_Data.FieldID)).with_only_columns([stmt.c.LangID]).order_by(case(value=stmt.c.LangID, whens={LangID: 0}, else_=1),stmt.c.LangID).offset(0).limit(1).as_scalar())).
-#            filter(models.Record_Data.NUM.in_([x[0] for x in results])).
-#            filter(models.Field.FieldName=='LOCATED_IN_CM').
-#            order_by(models.Record_Data.NUM)
-#           ).all()
-#
-#
-#        
-#        data = name_data + located_in_data
-#        data.sort(key=lambda x: x[0])
-#
-#        
-#        name_data = {k: {x[1]:x[2] for x in v} for (k,v) in groupby(name_data, lambda x: x[0])}
         log.debug('Return')
 
                      
-        return {'results': results}#, 'name_data': name_data}
+        return {'results': results}
 
 
 
